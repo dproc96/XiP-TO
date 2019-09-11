726057e3feff834d593e00f0569d9b7b1bb4b074
Dan Proctor <dproc96@gmail.com>
2019-09-11 16:48:50 -0400

    minor fixes to linting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,40 +25,43 @@ module.exports = function(app) {
     }
   });
 
-  // app.post("/api/experiences/new", function(request, response) {
-  //   let data = request.body;
-  //   // Add logic for determining correct data structure in place of true
-  //   if (true) {
-  //     // Add object for creating when database structure is known
-  //     db.Experiences.create({}).then(function(results) {
-  //       response.status(200).end()
-  //     }).catch(function(error) {
-  //       response.status(503).end()
-  //     })
-  //   } else {
-  //     response.status(400).end()
-  //   }
-  // })
+  app.post("/api/experiences/new", function(request, response) {
+    let data = request.body;
+    // Add logic for determining correct data structure in place of true
+    if (true) {
+      // Add object for creating when database structure is known
+      db.Experiences.create({data}).then(function(results) {
+        console.log(results);
+        response.status(200).end();
+      }).catch(function(error) {
+        console.log(error);
+        response.status(503).end();
+      });
+    } else {
+      response.status(400).end();
+    }
+  });
 
-  // app.put("/api/experiences/:id", function(request, response) {
-  //   let data = request.body;
-  //   // Add logic for determining correct data structure in place of true
-  //   if (true) {
-  //     // Add object for updating when database structure is known
-  //     db.Experiences.update({}, {
-  //       where: {
-  //         id: request.params.id
-  //       }
-  //     }).then(function(results) {
-  //       if (results.affectedRows === 0) {
-  //         response.status(404).end()
-  //       }
-  //       response.status(200).end()
-  //     }).catch(function(error) {
-  //       response.status(503).end()
-  //     })
-  //   } else {
-  //     response.status(400).end()
-  //   }
-  // })
+  app.put("/api/experiences/:id", function(request, response) {
+    let data = request.body;
+    // Add logic for determining correct data structure in place of true
+    if (true) {
+      // Add object for updating when database structure is known
+      db.Experiences.update({data}, {
+        where: {
+          id: request.params.id
+        }
+      }).then(function(results) {
+        if (results.affectedRows === 0) {
+          response.status(404).end();
+        }
+        response.status(200).end();
+      }).catch(function(error) {
+        console.log(error);
+        response.status(503).end();
+      });
+    } else {
+      response.status(400).end();
+    }
+  });
 };
