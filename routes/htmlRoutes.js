54efbaff5b5ed266a572af1db48fc14afc2ca1b2
Dan Proctor <dproc96@gmail.com>
2019-09-12 16:38:55 -0400

    Converted experience.html to new-experience.handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var metadata = require("../config/metadata");
 
 module.exports = function(app) {
   // Load index page
@@ -12,12 +13,10 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/experiences/:id", function(req, res) {
+    if (req.param.id = "new") {
+      res.render("new-experience", {metadata: metadata});
+    }
   });
 
   // Render 404 page for any unmatched routes
