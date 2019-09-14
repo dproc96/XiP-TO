ca30cef5a3b5448d77bc7c70b907f6c9ba62e56a
soheila <soheilalotfi@ymail.com>
2019-09-14 14:48:57 -0400

    add-signup-modal

--- public/js/logic.js
+++ public/js/logic.js
@@ -37,3 +37,69 @@ $(".slide").slick({
   prevArrow: "<button type='button' data-role='none' class='slick-prev slick-arrow' aria-label='Next' role='button' style='display: block;'>Next</button>"
 });
 
+
+
+$(document).ready(function() {
+  $(document).on("click", "#signUP", makeNewModal);
+
+  function makeNewModal(event) {
+    event.preventDefault();
+
+    $("#modal").empty();
+
+    var newModal = $("<modal>").addClass("modal");
+
+    var xIcon = $("<i>")
+      .attr("id", "close-modal")
+      .addClass("fas fa-times btn-close");
+    var firstNameRow = $("<input>").attr({
+      type: "text",
+      id: "inputFirstName",
+      placeholder: "First Name",
+      required: "true"
+    });
+    var lastNameRow = $("<input>").attr({
+      type: "text",
+      id: "inputlastName",
+      placeholder: "Last Name",
+      required: "true"
+    });
+
+    var emailRow = $("<input>").attr({
+      type: "email",
+      id: "inputEmail",
+      placeholder: "Email Address",
+      required: "true",
+      autofocus: "true"
+    });
+    var passswordRow = $("<input>").attr({
+      type: "text",
+      id: "inputPassword",
+      placeholder: "Password",
+      required: "true"
+    });
+    var reEnterPassswordRow = $("<input>").attr({
+      type: "text",
+      id: "reinputPassword",
+      placeholder: "ReEnter Password",
+      required: "true"
+    });
+   
+    var submitBtn = $("<button>")
+      .attr("id", "submitUserInformation")
+      .addClass("btn btn__large")
+      .text("Submit");
+
+    newModal.append(
+      xIcon,
+      firstNameRow,
+      lastNameRow,
+      emailRow,
+      passswordRow,
+      reEnterPassswordRow,
+      submitBtn
+    );
+
+    $("#modal").append(newModal);
+  }
+});
