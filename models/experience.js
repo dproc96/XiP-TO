2bb4228ec8bea11e821a75ad16ee7730ad5d1be3
Carolina Cavalcanti <carolinapc@gmail.com>
2019-09-14 10:19:46 -0400

    Add experience association with categories

--- models/experience.js
+++ models/experience.js
@@ -107,6 +107,12 @@ module.exports = function (sequelize, DataTypes) {
         allowNull: false
       }
     });
+
+    Experience.belongsTo(models.Category, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
   };  
   
   return Experience;
