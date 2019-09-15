1cb60576358236182c7cbf80b48f692ee9479915
Dan Proctor <dproc96@gmail.com>
2019-09-15 12:16:14 -0400

    bug fixes and cleaned up files/folders

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,11 +1,12 @@
 $(document).on("click", "#sign-in", function() {
   $("#overlay").attr("class", "overlay");
-  $("#modal").attr("class", "modal");
+  $("#sign-in-modal").attr("class", "modal");
 });
 
 $(document).on("click", "#close-modal", function() {
   $("#overlay").attr("class", "overlay hidden");
-  $("#modal").attr("class", "modal hidden");
+  $("#sign-in-modal").attr("class", "modal hidden");
+  $("#sign-up-modal").attr("class", "modal hidden");
 });
 
 $(document).on("click", "#menu", function() {
@@ -17,71 +18,78 @@ $(document).on("click", "#menu", function() {
   }
 });
 
-$(document).on("click", "#signUP", makeNewModal);
-
-$(".slide").slick({
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1500,
-  responsive: [{
-    breakpoint: 850,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      infinite: true,
-    }
-  }],
-  variableWidth: true,
-  nextArrow: "<button type='button' data-role='none' class='slick-next slick-arrow' aria-label='Next' role='button' style='display: block;'>Next</button>",
-  prevArrow: "<button type='button' data-role='none' class='slick-prev slick-arrow' aria-label='Next' role='button' style='display: block;'>Next</button>"
-
+$(document).on("click", "#signUp", function() {
+  $("#sign-in-modal").attr("class", "modal hidden");
+  $("#sign-up-modal").attr("class", "modal");
 });
+
+$(document).on("click", "#submitUserInformation", postUserInfo);
+
+if ($(".slide").length) {
+  $(".slide").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [{
+      breakpoint: 850,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    }],
+    variableWidth: true,
+    nextArrow: "<button type='button' data-role='none' class='slick-next slick-arrow' aria-label='Next' role='button' style='display: block;'>Next</button>",
+    prevArrow: "<button type='button' data-role='none' class='slick-prev slick-arrow' aria-label='Next' role='button' style='display: block;'>Next</button>"
+  });
+}
+
 
 function makeNewModal(event) {
   event.preventDefault();
 
   $("#modal").empty();
 
-  var newModal = $("<modal>").addClass("modal");
+  let newModal = $("<modal>").addClass("modal");
 
-  var xIcon = $("<i>")
+  let xIcon = $("<i>")
     .attr("id", "close-modal")
     .addClass("fas fa-times btn-close");
-  var firstNameRow = $("<input>").attr({
+  let firstNameRow = $("<input>").attr({
     type: "text",
     id: "inputFirstName",
     placeholder: "First Name",
     required: "true"
   });
-  var lastNameRow = $("<input>").attr({
+  let lastNameRow = $("<input>").attr({
     type: "text",
     id: "inputlastName",
     placeholder: "Last Name",
     required: "true"
   });
 
-  var emailRow = $("<input>").attr({
+  let emailRow = $("<input>").attr({
     type: "email",
     id: "inputEmail",
     placeholder: "Email Address",
     required: "true",
     autofocus: "true"
   });
-  var passswordRow = $("<input>").attr({
+  let passswordRow = $("<input>").attr({
     type: "text",
     id: "inputPassword",
     placeholder: "Password",
     required: "true"
   });
-  var reEnterPassswordRow = $("<input>").attr({
+  let reEnterPassswordRow = $("<input>").attr({
     type: "text",
     id: "reinputPassword",
     placeholder: "ReEnter Password",
     required: "true"
   });
 
-  var submitBtn = $("<a>")
+  let submitBtn = $("<a>")
     .attr("id", "submitUserInformation")
     .addClass("btn btn__large")
     .text("Submit");
@@ -100,24 +108,24 @@ function makeNewModal(event) {
 
 }
 
-$(document).on("click", "#submitUserInformation", postUserInfo);
 
 function postUserInfo(event) {
   event.preventDefault();
 
-  var newUserInfo={
-    firstname: $("#inputFirstName").val().trim(),
-    lastname: $("#inputlastName").val().trim(),
-    email: $("#inputEmail").val().trim(),
-    password: $("#inputPassword").val().trim(),
+  let newUserInfo={
+    firstname: $("#signUpFirstName").val().trim(),
+    lastname: $("#signUpLastName").val().trim(),
+    email: $("#signUpEmail").val().trim(),
+    password: $("#signUpPassword").val().trim(),
     score: 0,
     active: true
   };
-
-  $.post("/api/users", newUserInfo, function(){
-    location.reload();
-    
-  });
+  
+  if (newUserInfo.password === $("#reenterSignUpPassword").val()) {
+    $.post("/api/users", newUserInfo, function(){
+      location.reload();
+    });
+  }
 }
 
 
