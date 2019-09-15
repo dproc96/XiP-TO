aef5a9c8cbeffc099e20ce15bb5b4bc60d184030
Carolina Cavalcanti <carolinapc@gmail.com>
2019-09-15 03:14:39 -0400

    implemented the experiences update and improvements on the file upload

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -41,13 +41,36 @@ module.exports = function(app) {
 
   // Load experience form page and pass an experience id
   app.get("/experiences/:id", function(req, res) {
-    if (req.param.id = "new") {
+    if (req.params.id === "new") {
 
       //load categories from the database
       metadata.loadCategories().then(data => {
         metadata.categories = data;
         res.render("new-experience", { metadata: metadata });
       });
+      
+    }
+    else {
+      
+      db.Experience.findByPk(req.params.id).then(function (experience) {
+        //load categories from the database
+        metadata.loadCategories().then(function(data) {
+          
+          metadata.categories = data;
+          metadata.experience = experience;
+          res.render("new-experience", { metadata: metadata, experience: experience });
+        });
+        
+      }).catch(err => {
+        if (err.errors) {
+          res.status(500).end(err.errors[0].message);
+        }
+        else {
+          console.log(err);
+          res.status(500).end(err.message);
+        }
+      });
+      
       
     }
   });
